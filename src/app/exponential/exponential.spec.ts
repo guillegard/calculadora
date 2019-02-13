f0b9908509b4e70415ffeade1077a2d0f5f1b986
guillegard <guillegard95@gmail.com>
2019-02-13 14:05:19 -0600

    exponential and tests

---
+++ src/app/exponential/exponential.spec.ts
@@ -0,0 +1,21 @@
+import {exponential} from './exponential';
+
+describe('exponential', () =>{ 
+
+    it('it should have 2 numbers', () => {
+        expect(exponential(2,2)).toBe(4);
+    })
+
+    it('it should be 1 when second number is 0', () => {
+        expect(exponential(2,0)).toBe(1);
+    })
+
+    it('it should be 0 when first number is 0', () => {
+        expect(exponential(0,2)).toBe(0);
+    })
+
+    it('it should do the exponential', () => {
+        expect(exponential(5,5)).toBe(3125);
+    })
+    
+});
